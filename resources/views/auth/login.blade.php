a409cd933f3af13f9a7f100a1d867c8a9d3f374d
Mihir Lakhamje <lakhamjemihir@gmail.com>
2025-09-05 23:25:31 +0530

    UI: forget password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,8 +29,6 @@
         <div class="mb-5 flex justify-between">
             <span><a href="{{ route('register') }}"
                     class="text-sm text-gray-700 hover:underline dark:text-gray-400">Don't have an account?</a></span>
-            <span><a href="/" class="text-sm text-gray-700 hover:underline dark:text-gray-400">Forgot
-                    password?</a></span>
         </div>
         <button type="submit"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
